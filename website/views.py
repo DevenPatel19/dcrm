c881407d9ccdc1f1682ad5b8d9d1b71c34174d8a
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-08-22 15:47:44 -0400

    registration

--- website/views.py
+++ website/views.py
@@ -31,3 +31,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been successfully logged out")
     return redirect('home')
+
+
+def register_user(request):
+    return render(request, "register.html", {})
