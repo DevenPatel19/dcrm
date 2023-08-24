c2e2f448f97e5eba2cd43c5464ce2badef41a6f3
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-08-24 10:38:00 -0400

    delete user

--- website/views.py
+++ website/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == "POST":
         username = request.POST.get("username")
@@ -26,7 +29,7 @@ def home(request):
             return redirect('home')
     else:
 
-        return render(request, "home.html", {})
+        return render(request, "home.html", {'records': records})
 
 
 def logout_user(request):
@@ -52,3 +55,23 @@ def register_user(request):
         return render(request, "register.html", {'form': form})
 
     return render(request, "register.html", {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Successfully Deleted")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to Delete")
+        return redirect('home')
