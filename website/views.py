ce79fc54a8dbba7fea0863e93ab0b9253c42a96b
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-08-24 20:50:51 -0400

    MVP project basic functioning CRM

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
+
 
 # Create your views here.
 
@@ -74,4 +75,32 @@ def delete_record(request, pk):
         return redirect('home')
     else:
         messages.success(request, "You must be logged in to Delete")
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Successfully Added")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "You Must Be Logged In to Add a Record")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Successfully Updated")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You Must Be Logged In to Update a Record")
         return redirect('home')
